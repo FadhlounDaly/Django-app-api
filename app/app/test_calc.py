293694c32de8ddb0402e59876614e2ec48c414f9
FadhlounDaly <daly@Dalys-MacBook-Pro.local>
2022-09-30 20:49:54 +0900

    fixing database race condition linting 2

--- app/app/test_calc.py
+++ app/app/test_calc.py
@@ -9,15 +9,16 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
     """Test the calc module"""
 
     def test_add_numbers(self):
         """Testing adding numbers together."""
-        res= calc.add(5, 6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         """Testing subtracting numbers"""
         res = calc.subtract(10, 5)
