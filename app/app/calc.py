e166fc3641a3e12775470bbf001c7d4e89843cce
FadhlounDaly <daly@Dalys-MacBook-Pro.local>
2022-09-30 20:34:47 +0900

    fixing database race condition linting

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+
+calculator function
+
+"""
+
+def add(x,y):
+    """Add x and y and return the result."""
+    return x + y
+
+def subtract(x,y):
+    """subtract x from y and return the result."""
+    return x - y
