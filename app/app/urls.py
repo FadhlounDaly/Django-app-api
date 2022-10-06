61ffbd2882e23cd95f1c23fc95e2b6450a1ffb1a
FadhlounDaly <daly@Dalys-MacBook-Pro.local>
2022-10-06 21:47:00 +0900

    added user create API (Unit test).

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -28,6 +29,6 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-    )
-
+    ),
+    path('api/user/', include('user.urls')),
 ]
